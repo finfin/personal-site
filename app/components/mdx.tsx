729d9c2a3acc7964f1b15931f70c0931b4614472
fin <finfin@gmail.com>
2024-12-10 18:18:22 +0800

    post auto heading link and positioning

--- app/components/mdx.tsx
+++ app/components/mdx.tsx
@@ -3,8 +3,9 @@ import Image from 'next/image'
 import { MDXRemote } from 'next-mdx-remote/rsc'
 import React from 'react'
 import { CodePen } from '@/components/codepen'
-
 import '@/styles/highlight-monokai.css'
+import { cn } from '@/lib/utils'
+
 function Table({ data }) {
   const headers = data.headers.map((header, index) => (
     <th key={index}>{header}</th>
@@ -49,31 +50,11 @@ function RoundedImage(props) {
   return <Image alt={props.alt} className="rounded-lg" {...props} />
 }
 
-
-function slugify(str) {
-  return str
-    .toString()
-    .toLowerCase()
-    .trim() // Remove whitespace from both ends of a string
-    .replace(/\s+/g, '-') // Replace spaces with -
-    .replace(/&/g, '-and-') // Replace & with 'and'
-    .replace(/[^\w-]+/g, '') // Remove all non-word characters except for -
-    .replace(/--+/g, '-') // Replace multiple - with single -
-}
-
 function createHeading(level) {
-  const Heading = ({ children }) => {
-    const slug = slugify(children)
+  const Heading = ({ children, className, ...props }) => {
     return React.createElement(
       `h${level}`,
-      { id: slug },
-      [
-        React.createElement('a', {
-          href: `#${slug}`,
-          key: `link-${slug}`,
-          className: 'anchor',
-        }),
-      ],
+      { className: cn('scroll-mt-20', className), ...props },
       children
     )
   }
--- contentlayer.config.ts
+++ contentlayer.config.ts
@@ -2,7 +2,7 @@
 import { defineDocumentType, makeSource } from 'contentlayer2/source-files'
 import highlight from 'rehype-highlight'
 import rehypeSlug from 'rehype-slug'
-import remarkCustomHeaderId from 'remark-custom-header-id'
+import rehypeAutolinkHeadings from 'rehype-autolink-headings'
 
 export const Post = defineDocumentType(() => ({
   name: 'Post',
@@ -74,9 +74,15 @@ export default makeSource({
   contentDirPath: 'content',
   documentTypes: [Post, Page],
   mdx: {
-    remarkPlugins: [remarkCustomHeaderId],
     rehypePlugins: [
       rehypeSlug,
+      [rehypeAutolinkHeadings, {
+        behavior: 'prepend',
+        properties: {
+          className: ['anchor'],
+          ariaLabel: 'Anchor',
+        },
+      }],
       highlight,
     ],
   },
